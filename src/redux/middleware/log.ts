949c1e8c969d09ff66a690a33b568f0d9d5adff7
rizqiagfian <rizqiagfian14@gmail.com>
2023-03-22 12:38:29 +0700

    drop

--- src/redux/middleware/log.ts
+++
@@ -1,7 +0,0 @@
-import { Middleware, Dispatch, Action } from "@reduxjs/toolkit";
-
-const log: Middleware = (store) => (next: Dispatch) => (action: Action) => {
-    console.log(action);
-    next(action);
-};
-export default log;
